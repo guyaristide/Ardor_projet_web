d8e30fa22c0233cf4aab54d9cf2cbd9f489b7089
guyaristide <guyaristide@gmail.com>
2015-11-03 18:00:01 +0000

    Suite generation
    Géneration des fichiers essentiel dans la logique du code

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
  * Les liens seron sous la forme : ex1: nomdusite.ci?module=accueil
  *                                  ex2: nomdusie.ci?module=profil&action=connexion
@@ -8,6 +7,55 @@
  * Pour afficher une page le module ddoit existé puis l'action aussi
  * si le module n'existe pas on affiche une page par defaut sinon
  * on se rend au module et on cherche l'action
+ * 
+ * 
  */
 
 
+include './brain/init.php';
+
+$head = "global/head.php";
+
+//démarage de la temporisation
+
+ob_start();
+
+//Première vérification 
+//Existance de module dans url
+
+
+if (isset($_GET["module"])) {
+
+    //deuxieme vérificaion existance du module sur espace serveur
+
+    /*
+     * Cette partie est a remplacée quand les fonction dans brain seron disponible
+     */
+    
+    $module = __DIR__ . "/modules/" . $_GET["module"] . "/" . $_GET["module"] . ".php";
+    if (is_file($module)) {
+        include $module;
+    }
+    //sinon module par defaut
+    else {
+        include "";
+    }
+}
+//sinon module par defaut
+else {
+    include "";
+}
+
+//fin de la temporisation
+
+
+    $content = ob_get_clean();
+
+
+//Debut du rendu
+
+    include $head;
+
+
+    echo $content;
+?>
---
+++ modules/accueil/accueil.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 
+ * Moteur tout acces au module accueil passe par cette page 
+ * c'est lindex du module accueil
+ * 
+ * 
+ * 
+ * Elle commence par verifier si l'action demandé dans le lien existe sinon elle appel la page par defaut
+ * du dossier vue
+ */
+
